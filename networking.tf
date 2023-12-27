50cffddc63f4b405c990210ca0a300917a55ee8c
gopal <gopal1409@gmail.com>
2023-12-27 18:49:40 +0530

    tags

--- networking.tf
+++ networking.tf
@@ -6,3 +6,19 @@ module "my_vpc" {
 }
 
 ###igw
+module "igw" {
+  source = "./modules/internet_gw"
+  vpc_id = module.my_vpc.my_vpc_id
+  #we map the vpc id from the output.tf file inside vpc module folder
+  #output "my_vpc_id" {
+  #value = aws_vpc.my_vpc.my_vpc_id
+ #}
+  tags_igw = var.tags_igw
+}
+
+module "eip" {
+    source = "./modules/elastic_ip"
+    tags_eip = var.tags_eip 
+    igw = module.igw   
+}
+
